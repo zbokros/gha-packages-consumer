7ba35103a8186eb26e2f4d1f74d72e7ae976fdad
Zoltán Bokros <zoltan.bokros@adevinta.com>
2023-11-30 13:46:51 +0100

    Init lib

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.mylibrary.TestClass
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +27,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        TestClass().asd()
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,21 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            name = "GitHubPackages"
+
+            // Replace GITHUB_USERID with your personal or organisation user ID and
+            // REPOSITORY with the name of the repository on GitHub
+            url = uri("https://maven.pkg.github.com/zbokros/github-packages-demo")
+
+            credentials {
+                username = "22410777"
+                password = "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"
+            }
+        }
     }
 }
 
+
 rootProject.name = "My Application"
 include(":app")
- 
